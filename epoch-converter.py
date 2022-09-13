fc1df3dcfc39019fa9d321295466415c0e579f93
Panda <60609220+PandaMcGame@users.noreply.github.com>
2022-09-14 00:52:51 +0800

    Update epoch-converter.py

--- epoch-converter.py
+++ epoch-converter.py
@@ -1,4 +1,11 @@
-import time
+from calendar import timegm
+from datetime import datetime
 
 def convert_epoch(time):
+    """Convert Epoch to Human Time"""
     return datetime.fromtimestamp(float(time)/1000).strftime('%Y-%m-%d %H:%M:%S.%f')
+
+
+def convert_human():
+    """Conert Human Time to Epoch"""
+    return calendar.timegm(datetime.datetime.now().timetuple())
