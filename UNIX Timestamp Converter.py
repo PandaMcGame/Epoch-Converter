b14c7ab4d9a69f0b5a833645447df08afda08ced
Panda <60609220+PandaMcGame@users.noreply.github.com>
2023-10-11 12:37:40 +0800

    Initial Commit

--- UNIX Timestamp Converter.py
+++ UNIX Timestamp Converter.py
@@ -1,11 +1,27 @@
-from calendar import timegm
-from datetime import datetime
-
-def convert_epoch(time):
-    """Convert Epoch to Human Time"""
-    return datetime.fromtimestamp(float(time)/1000).strftime('%Y-%m-%d %H:%M:%S.%f')
+import time
 
 
-def convert_human():
-    """Conert Human Time to Epoch"""
-    return calendar.timegm(datetime.now().timetuple())
+def epoch_to_datetime(epoch_time: int) -> str:
+    """
+    Convert Epoch time to a formatted datetime string.
+
+    Args:
+        epoch_time (int): The Epoch time to convert.
+
+    Returns:
+        str: A formatted datetime string (e.g., 'YYYY-MM-DD HH:MM:SS').
+    """
+    return time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(epoch_time))
+
+
+def datetime_to_epoch(datetime_str: str) -> int:
+    """
+    Convert a formatted datetime string to Epoch time.
+
+    Args:
+        datetime_str (str): A formatted datetime string (e.g., 'YYYY-MM-DD HH:MM:SS').
+
+    Returns:
+        int: The corresponding Epoch time.
+    """
+    return int(time.mktime(time.strptime(datetime_str, '%Y-%m-%d %H:%M:%S'))
